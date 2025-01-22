a18cce8bb65bf2dd2d98e585bd329020366867b4
Christian Martiny <christian.martiny@outlook.com>
2025-01-22 12:15:28 +0100

    sdf

--- app/testing.py
+++ app/testing.py
@@ -1,3 +1,19 @@
 # ONLY FOR TESTING WITHOUT DOCKER
-from utils import *
+import utils
 
+# species	island	bill_length_mm	bill_depth_mm	flipper_length_mm	body_mass_g	sex
+# 0	Adelie	Torgersen	39.1	18.7	181.0	3750.0	Male
+# 1	Adelie	Torgersen	39.5	17.4	186.0	3800.0	Female
+# species	island	bill_length_mm	bill_depth_mm	flipper_length_mm	body_mass_g	sex
+# 214	Gentoo	Biscoe	46.1	13.2	211.0	4500.0	Female
+# 215	Gentoo	Biscoe	50.0	16.3	230.0	5700.0	Male
+# species	island	bill_length_mm	bill_depth_mm	flipper_length_mm	body_mass_g	sex
+# 146	Chinstrap	Dream	46.5	17.9	192.0	3500.0	Female
+# 147	Chinstrap	Dream	50.0	19.5	196.0	3900.0	Male
+
+print(utils.predict("Torgersen", 39.1, 18.7, 181.0, 3750.0, "Male"))
+print(utils.predict("Torgersen", 39.5, 17.4, 186.0, 3800.0, "Female"))
+print(utils.predict("Biscoe", 46.1, 13.2, 211.0, 4500.0, "Female"))
+print(utils.predict("Biscoe", 50.0, 16.3, 230.0, 5700.0, "Male"))
+print(utils.predict("Dream", 46.5, 17.9, 192.0, 3500.0, "Female"))
+print(utils.predict("Dream", 50.0, 19.5, 196., 3900.0, "Male"))
--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,6 @@
-import numpy as np
 import pandas as pd
-import pingouin as pg
+from joblib import load
+from logs import init_log, logging_msg
 
 
 
@@ -8,3 +8,57 @@ import pingouin as pg
 ####################################################################################################
 ####################################################################################################
 
+############
+### INIT ###
+############
+def init()->bool:
+    try:
+        if init_log() == False:
+            raise Exception("Error in utils.py init(): init_log() failed")
+        return True
+    
+    except Exception as e:
+        print(e)
+        return False
+
+####################################################################################################
+####################################################################################################
+####################################################################################################
+
+def predict(
+        island: str,
+        bill_length_mm: float,
+        bill_depth_mm: float,
+        flipper_length_mm: float,
+        body_mass_g: float,
+        sex: str)->str:
+    log_prefix = '[utils | predict]'
+    try:
+        if init() == False:
+            raise Exception("Error in utils.py predict(): init() failed")
+
+        # MODEL
+        model = load('./app/model_model.pkl')
+        scaler = load('./app/model_scaler.pkl')
+
+        # DATA
+        data = {
+            'island': ['Biscoe', 'Dream', 'Torgersen', island],
+            'bill_length_mm': [0, 1, 2, bill_length_mm],
+            'bill_depth_mm': [3, 4, 5, bill_depth_mm],
+            'flipper_length_mm': [6, 7, 8, flipper_length_mm],
+            'body_mass_g': [9, 10, 11, body_mass_g],
+            'sex': ['Female', 'Male', 'Female', sex]
+        }
+        X = pd.DataFrame(data)
+        
+        # PREDICTION
+        X_dummies = pd.get_dummies(X)
+        X_valid_sample_transformed = scaler.transform([X_dummies.iloc[3]])
+        y_pred_sample = model.predict(X_valid_sample_transformed)
+
+        return y_pred_sample[0]
+    
+    except Exception as e:
+        logging_msg(f"{log_prefix} {e}", 'ERROR')
+        return None
